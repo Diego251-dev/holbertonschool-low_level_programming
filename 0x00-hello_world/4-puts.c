655d4f618187b064a1f4e731621de89da766faa2
Diego251-dev <2072@holbertonschool.com>
2020-09-27 16:31:14 -0500

    PutsC

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *Main - Print with Puts
+ *Programming is like building a multilingual puzzle
+ *return 0
+ */
+int main(void) 
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
