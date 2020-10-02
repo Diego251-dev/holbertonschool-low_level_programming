d15caeb1a6277cb8315d54ffc27805641f97fe98
Diego251-dev <2072@holbertonschool.com>
2020-10-01 21:44:16 -0500

    Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Print Holberton
+ *
+ * Return: 0 Always (Success)
+ */
 int main(void)
 {
-  printf("Holberton\n");
-  return (0);
+printf("Holberton\n");
+return (0);
 }
