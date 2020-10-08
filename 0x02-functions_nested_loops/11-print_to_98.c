4129c7e864972a538dfd4b5191b04476247a46a7
Diego251-dev <2072@holbertonschool.com>
2020-10-07 20:32:38 -0500

    printf to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,11 +6,19 @@
  */
 void print_to_98(int n)
 {
+
 int i;
 
-if (n  <= 98)
+if (n <= 98)
 {
 for (i = n; i < 98; i++)
+{
+printf("%i, ", i);
+}
+}
+else
+{
+for (i = n; i > 98; i--)
 {
 printf("%i, ", i);
 }
