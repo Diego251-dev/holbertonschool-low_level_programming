7c47d610b683f817e16777f269c79f1068da4bcf
Diego251-dev <2072@holbertonschool.com>
2020-10-09 14:35:30 -0500

    Print

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* print_most_numbers - check the code for Holberton School students.
+* Desc: Entry
+* Return: Always 0.
+*/
+void print_most_numbers(void)
+{
+char c;
+for (c = 0; c <= 9; c++)
+if (c != 2 && c != 4)
+_putchar(c);
+_putchar('\n');
+}
