bb35d7d53a536c5e9ab0c3e015a8bb7cc7bd6290
Diego251-dev <2072@holbertonschool.com>
2020-10-11 16:03:18 -0500

    Print Numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,9 +5,10 @@
  */
 void print_numbers(void)
 {
-int i;
-
-for (i = 0; i < 10; i++)
-_putchar(i + 48);
-_putchar('\n');
+  char n;
+  for (n = '0' ; n <= '9'; n++);
+  {
+    _putchar(n);
+  }
+  _putchar('\n');
 }
