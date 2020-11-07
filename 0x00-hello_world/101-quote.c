23cf8f6b453c162e130aa8ae2c129440a7b364e4
Diego251-dev <2072@holbertonschool.com>
2020-11-06 21:57:56 -0500

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Prints string
@@ -8,5 +7,5 @@
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-return (0);
+return (1);
 }
