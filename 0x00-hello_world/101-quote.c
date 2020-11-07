fa422dd69644ec2e4d951eec5a20a86fa3b55e5f
Diego251-dev <2072@holbertonschool.com>
2020-11-06 22:25:11 -0500

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Prints string
  * Description: Prints "\"Programming is like building a multilingual puzzle"
  * Return: 0
  */
-int main()
+int main(void)
 {
 write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 return (1);
