d3b1f09ff2db632a536f3ed9d292be1d2e26c3fc
Diego251-dev <2072@holbertonschool.com>
2020-11-08 15:58:44 -0500

    Strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* _strcat - The source
+*@src: integer *
+*@dest: integer *
+* Return: dest
+*/
+char *_strcat(char *dest, char *src)
+{
+int i;
+for (i = 0; dest[i] != '\0'; i++)
+{
+}
+for (int b = 0; src[b] != '\0'; b++, i++)
+{
+dest[i] = src[b];
+}
+return (dest);
+}
