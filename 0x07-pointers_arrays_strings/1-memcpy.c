d75413c530a1fe7f3c0e18e0acf61a961ec2b7c8
Diego251-dev <2072@holbertonschool.com>
2020-11-14 16:19:50 -0500

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,17 @@
 #include "holberton.h"
-
+/**
+ * _memcpy - copy n bytes from src to dest
+ * @dest: dest from src
+ * @src: src to dest
+ * @n: string n
+ * Return: _memcpy
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    
+unsigned int c;
+for (c = 0; c < n; c++)
+{
+dest[c] = src[c];
+}
+return (dest);
 }
