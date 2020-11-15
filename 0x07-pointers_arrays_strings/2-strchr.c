c3d929c0e97dd0e827078b5260ce7afbebbac34d
Diego251-dev <2072@holbertonschool.com>
2020-11-14 19:37:57 -0500

    Strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strchr - Return a pointer
+ * @s: The srting
+ * @c: Character
+ * Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+{
+return (s + i);
+}
+}
+if (s[i] == c)
+{
+return (s + i);
+}
+return (0);
+}
