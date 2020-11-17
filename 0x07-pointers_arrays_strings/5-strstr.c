5cf0e5795a2f146a7cdd5a85caed13c1dc3d5a3a
Diego251-dev <2072@holbertonschool.com>
2020-11-17 15:07:20 -0500

    Sisirve? :,v

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,25 +8,22 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-int i;
-int j;
-int k;
-for (i = 0; haystack[i] != '\0'; i++)
-{
-for (j = 0; needle[j] != '\0'; j++)
-{
-for (k = 0; haystack[i] <= needle[j]; k++)
-{
-if (haystack[k] != needle[j] || haystack[k] == '\0')
-{
-return (needle);
-}
-if (needle[j] == 0)
-{
-return (0);
-}
-}
-}
-}
-return (0);
+    int i;
+    int j;
+    int k;
+        for (i = 0; haystack[i]; i++)
+        {
+            for (k = i, j = 0; needle[j]; j++, k++)
+            {
+                    if (haystack[k] != needle[j] || haystack[k] == '\0')
+                    {
+                        break;
+                    }
+            }
+            if (needle[j] == 0)
+            {
+                return (needle);
+            }
+        }
+    return (0);
 }
